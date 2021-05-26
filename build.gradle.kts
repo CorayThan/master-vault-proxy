68c24314d5a09e2bf99bdd30d1ebd9572b8982bb
Nathan Westlake <coraythan@gmail.com>
2021-05-25 18:44:34 -0700

    fix bug with mav / enhanced / anomaly

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "coraythan"
-version = "5"
+version = "6"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
--- src/main/kotlin/coraythan/mvproxy/models/KeyForgeCard.kt
+++ src/main/kotlin/coraythan/mvproxy/models/KeyForgeCard.kt
@@ -1,24 +1,28 @@
 package coraythan.mvproxy.models
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class KeyForgeCard(
-        val id: String,
-        val card_title: String,
-        val house: String,
-        val card_type: String,
-        val front_image: String,
-        val card_text: String,
-        val amber: Int,
-        val power: String?,
-        val armor: String?,
-        val rarity: String,
-        val flavor_text: String? = null,
-        val card_number: String,
-        val expansion: Int,
-        val is_maverick: Boolean,
-        val is_anomaly: Boolean,
-        val is_enhanced: Boolean,
-        val traits: String? = null
+    val id: String,
+    val card_title: String,
+    val house: String,
+    val card_type: String,
+    val front_image: String,
+    val card_text: String,
+    val amber: Int,
+    val power: String?,
+    val armor: String?,
+    val rarity: String,
+    val flavor_text: String? = null,
+    val card_number: String,
+    val expansion: Int,
+    @get:JsonProperty("is_maverick")
+    val is_maverick: Boolean,
+    @get:JsonProperty("is_anomaly")
+    val is_anomaly: Boolean,
+    @get:JsonProperty("is_enhanced")
+    val is_enhanced: Boolean,
+    val traits: String? = null
 )
